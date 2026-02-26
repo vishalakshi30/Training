fef3ab3d2823fd6bf9a3e62a8284993394acb395
Vishalakshi30 <vishalu.koti@gmail.com>
2026-02-26 16:48:51 +0530

    addedd 3 days session code

---
+++ src/patterns/HollowSquare.java
@@ -0,0 +1,20 @@
+package patterns;
+
+public class HollowSquare {
+
+        public static void main(String[] args) {
+
+            int row = 5;
+            int col = 6;
+            for (int i = 1; i <=row; i++) {
+                for (int j = 1; j <= col; j++)
+                    if((i==1 || i==col) || (j==1 || j==col))
+                    {System.out.print("*");}
+                    else
+                    {System.out.print(" ");}
+
+                System.out.println();
+
+            }
+        }}
+
---
+++ src/patterns/Rhombous.java
@@ -0,0 +1,14 @@
+package patterns;
+
+public class Rhombous {
+        public static void main(String []as) {
+            int n= 4;
+            for(int i=1; i<=n; i++){
+                for (int j=1; j<i; j++){
+                    System.out.print(" ");
+                }
+                System.out.println("****");
+            }
+        }
+    }
+
